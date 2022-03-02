87153812ecf268a7bd7ebc24807252dd0b84870c
Rowena Schmidt <schmidt.rowena@gmail.com>
2022-03-02 11:29:30 +0100

    fix collision and fix delete bottomline

--- js/index.js
+++ js/index.js
@@ -20,7 +20,6 @@ let brickImg1, brickImg2, brickImg3, brickImg4;
 let board = [];             // nötig am Ende?
 let bricks = [];
 let lockedBricks = [];
-let collision;
 let startVelocity = 50;  //frameRate -> to increase later on
 
 
@@ -32,7 +31,8 @@ function preload() {
     brickImg4 = loadImage("/assets/Brick4-resized.jpg");
 }
 
-function setup() {  
+function setup() {
+    noLoop(); 
     frameRate(startVelocity);  //nach Zeit oder gelöschten Reihen oder ANzahl Steine insgesamt...steigt Tempo...!
     let wall = createCanvas(320, 200);
     //wall.position(300, 159, 'fixed');  fix canvas inside gap, aber nicht responsive..ändert sich mit größe browser
@@ -71,8 +71,8 @@ function draw() {
     const brick = bricks[bricks.length - 1];
     brick.move();
 
-    lockedBricks = bricks.slice(0, bricks.length - 1);
-    collision = brick.collidesWith(lockedBricks);
+    lockedBricks = bricks.slice(0, bricks.length - 1); //wird in jedem Drasloop geupdated
+    const collision = brick.collidesWith(lockedBricks);
     if ( brick.onGround() || collision.booleanResult ) { 
         if (collision.booleanResult && (brick.x === collision.brick.x)) {
             brick.y = collision.brick.y - brickHeight;       
@@ -80,8 +80,20 @@ function draw() {
         //create new brick at the end of array:
         newBrick();
     };
+       
+    deleteFullBaseline();
 
+    if (lockedBricks.length > 10 && lockedBricks[lockedBricks.length - 1].y === 0) {
+        endingTheGame(); // oder gameIsOver = true;
+    }
+   
+    // dann am ende nochmal check: // brauch ich das?? brauch ich gameIs true??:::
+    if (gameIsOver) {
+        endingTheGame();
+    };
+}
 
+function deleteFullBaseline() {
     //ab hier eine function, die selbstständig stehen kann:
     //check for baseline row filled: (Game is for now constructed in a way that only the bottom can be filled)
     let bricksOnBaseline = 0;
@@ -93,35 +105,14 @@ function draw() {
     //ich darf nicht letzten stein aus bricks nehmen,,,,!!! aber beide arrays ändern??
     //Workaround: bricks = last one of bricks und geänderterfallenbricks?
     if (bricksOnBaseline === 8) {
-        lockedBricks = lockedBricks.filter( brick => brick.y !== 180);
-        // später hier noch SOUND einfügen: "FREEDOM" & the hoff erscheint (< 3 sec snippit)?? oder aufleuchten...
-
         bricks = bricks.filter( brick => brick.y !== 180);
-        // why does this not work? throws an error at draw...zu schnell? in der zeit noch nicht gerechnet
-        //changes the whole element not just the y property...
+        
+        bricks.slice(0, bricks.length - 1).forEach(remainingBrick => remainingBrick.y += brickHeight); // gibt nichts zurück!!!
 
-        // bricks = bricks.filter( brick => brick.y !== 180).map(remainingBrick => remainingBrick.y += brickHeight);
-
-        console.log(lockedBricks);
-
-        //take everything else a notch down:
-
-        // lockedBricks = lockedBricks.forEach(remainingBrick => remainingBrick.y += brickHeight); // oder nur LockedBricks??? //löscht alle!
-        // bricks = bricks.forEach(remainingBrick => remainingBrick.y += brickHeight);
-        // console.log(lockedBricks); //changes the whole array
-         // brauch ich diese Zeile?
+        // später hier noch SOUND einfügen: "FREEDOM" & the hoff erscheint (< 3 sec snippit)?? oder aufleuchten...
     } 
-       
-
-    if (lockedBricks.length > 10 && lockedBricks[lockedBricks.length - 1].y === 0) {
-        endingTheGame(); // oder gameIsOver = true;
-    }
-   
-    // dann am ende nochmal check: // brauch ich das?? brauch ich gameIs true??:::
-    if (gameIsOver) {
-        endingTheGame();
-    };
 }
+
 
 function newBrick () {
     let imageArray = [brickImg1, brickImg2, brickImg3, brickImg4];
@@ -134,19 +125,26 @@ function keyPressed() {
     const brick = bricks[bricks.length - 1]; //always last element of array (hence, the active brick!)
 
     // check for collision with other bricks left or right:
-    collision = brick.collidesWith(lockedBricks);
-
+    
     //Left Arrow
     if (keyCode === 37) {
-        if ((brick.x > 0) && !(collision.booleanResult && (brick.y === collision.brick.y))) { // wie check nur nach collision on x???
+        if ((brick.x > 0)) {
             brick.x -= brickWidth;
-        }
+            const possibleCollisionAfterMovingLeft = brick.collidesWith(lockedBricks);
+            if (possibleCollisionAfterMovingLeft.booleanResult) {
+                brick.x += brickWidth;
+            };
+        };
     }
 
     // Right Arrow
     if (keyCode === 39) {
-        if ((brick.x < width - brickWidth) && !(collision.booleanResult && (brick.y === collision.brick.y))) { 
+        if (brick.x < width - brickWidth) { 
             brick.x += brickWidth;
+            const possibleCollisionAfterMovingRight = brick.collidesWith(lockedBricks);
+            if (possibleCollisionAfterMovingRight.booleanResult) {
+                brick.x -= brickWidth;
+            };
         };
     };
 }
@@ -235,17 +233,7 @@ window.addEventListener("load", () => {
         gameIntro.style.display = "none";
         gameBoard.style.display = "flex";
         gameOver.style.display = "none";
-
-        /* 
-        if (timerId) {
-            timerId 0 null
-        } else {
-            draw()
-            timerId = setInterval.....
-        }
-        }
-        
-        */
+        loop();
     });
   
     //listener on the RE-START button to hide the GAME OVER screen and show the game canvas
